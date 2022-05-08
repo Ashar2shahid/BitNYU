066bd861512129feb38730bf13696155ea59a1a3
Ashar <Ashar2shahid@gmail.com>
2022-05-07 23:26:13 -0400

    update blockchain file

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -301,4 +301,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(threaded=True, port=port, debug=True)
