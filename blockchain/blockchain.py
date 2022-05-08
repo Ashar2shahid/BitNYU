face8aecc308cf8d95ef5ad28d3ac1c70f598497
Ashar <Ashar2shahid@gmail.com>
2022-05-07 23:41:03 -0400

    heroku installation

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -296,9 +296,8 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5001,
+    parser.add_argument('-p', '--port', default=5000,
                         type=int, help="port to listen to")
     args = parser.parse_args()
     port = args.port
-
-    app.run(threaded=True, port=port, debug=True)
+    app.run(threaded=True, port=port)
